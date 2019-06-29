35a21870b03f12443bda99200a53f4dbdf73dbca
Arum Lee <sarada032@gmail.com>
2019-06-28 21:47:08 -0400

    fix router

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-import globalRouter from './routers/globalRouter'
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
-import routes from './routes';
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express();
 
 app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(helmet())
+app.use(helmet());
 app.use(morgan("dev"));
 
 app.use(routes.home, globalRouter);
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,18 @@
 import express from "express";
 import routes from "../routes";
-import { videos, vedioDetail, videoEdit, videoDelete, upload } from "../controllers/videoController";
+import {
+    videos,
+    vedioDetail,
+    videoEdit,
+    videoDelete,
+    upload
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.vedioDetail, vedioDetail);
 videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.vedioDetail, vedioDetail);
 videoRouter.get(routes.videoEdit, videoEdit);
 videoRouter.get(routes.videoDelete, videoDelete);
 
